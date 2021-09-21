159a699c01930f57eb7cf147b2e46a0d2e166842
eldad <eldad@arpalus.com>
2021-09-21 15:35:56 +0300

    .

--- vm_commands.py
+++ vm_commands.py
@@ -4,6 +4,35 @@ import os,subprocess,json
 from constants import *
 from google.cloud import storage
 
+def list_all_buckets_names():
+    os.system('gsutil ls')
+
+def list_bucket_files(bucket_name) : 
+    os.system(f'gsutil ls -r gs://{bucket_name}/**')
+
+def copy_blob(bucket_name, blob_name, destination_bucket_name, destination_blob_name):
+
+    """Copies a blob from one bucket to another with a new name."""
+    # bucket_name = "your-bucket-name"
+    # blob_name = "your-object-name"
+    # destination_bucket_name = "destination-bucket-name"
+    # destination_blob_name = "destination-object-name"
+
+    storage_client = storage.Client()
+
+    source_bucket = storage_client.bucket(bucket_name)
+    source_blob = source_bucket.blob(blob_name)
+    destination_bucket = storage_client.bucket(destination_bucket_name)
+
+    blob_copy = source_bucket.copy_blob(source_blob, destination_bucket, destination_blob_name)
+
+    print("Blob {} in bucket {} copied to blob {} in bucket {}.".format(
+            source_blob.name,
+            source_bucket.name,
+            blob_copy.name,
+            destination_bucket.name))
+
+
 def get_vm_data_csv(csv_filename):
     df = pd.read_csv(csv_filename)
     df = df[['instance','zone']]
