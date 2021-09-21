6e1a2892e848503cba375156c5a61b4a3adc436f
eldad <eldad@arpalus.com>
2021-09-21 22:12:28 +0300

    Revert "."
    This reverts commit 4ee8c17b9f528069a17b8c2eca21bc9721999032.

--- vm_commands.py
+++ vm_commands.py
@@ -4,38 +4,11 @@ import os,subprocess,json,shutil
 from constants import *
 from google.cloud import storage
 
-#############################################################################################################################
-#Deprecated funcs
-def send_script_to_vm_by_vm_number(df,vm_number,src_path = None , dest_path = None): 
-    #deprecated for now
-    instance_name , zone = get_vm_attributes_from_vm_number(df, vm_number)
-    if src_path == None : src_path = './pull_docker.sh'
-    if dest_path == None : dest_path = f'{instance_name}:~'
-    return os.system(f'gcloud compute scp --project {PROJECT_NAME} --zone {zone} {src_path} {dest_path}')
-
-def send_script_to_all_vms_by_vm_numbers(df):
-    #deprecated for now
-    #currently serial and not parallel
-    df['vm_number'] = df['instance'].apply(lambda x : str(int(x.split(sep='-')[-1].strip())))
-    vm_numbers = df['vm_number'].to_list()
-    [send_script_to_vm_by_vm_number(df,vm_number) for vm_number in vm_numbers]
-
-
-def ssh_to_machine_via_number(vm_number:int): 
-    #deprecated for now
-    return os.system(SSH_COMMAND_BY_VM_NUMBER_DICT[f'{vm_number}'])
-
-def start_and_connect_to_vm(vm_number:int):
-    #deprecated for now
-    start_specific_vms([f'{vm_number}'])
-    ssh_to_machine_via_number(vm_number)
-#############################################################################################################################
+def list_all_buckets_names():
+    os.system('gsutil ls')
 
 def list_bucket_files(bucket_name) : 
     os.system(f'gsutil ls -r gs://{bucket_name}/**')
-
-def list_all_buckets_names():
-    os.system('gsutil ls')
 
 def copy_blob(bucket_name, blob_name, destination_bucket_name, destination_blob_name):
 
@@ -59,6 +32,7 @@ def copy_blob(bucket_name, blob_name, destination_bucket_name, destination_blob_
             blob_copy.name,
             destination_bucket.name))
 
+
 def get_vm_data_csv(csv_filename):
     df = pd.read_csv(csv_filename)
     df = df[['instance','zone']]
@@ -67,10 +41,17 @@ def get_vm_data_csv(csv_filename):
     insatnces = df['instance'].to_list()  
     zones = df['zone'].to_list()
     return df, insatnces,zones
-
+    
 def start_command(instance_name_str,zone_str):
     command = f'gcloud compute instances start --zone {zone_str}  {instance_name_str}&'
     os.system(command)
+
+def start_all_vms(instances,zones):
+    [start_command(instance_name,instance_zone) for (instance_name,instance_zone) in zip(instances,zones)]
+
+def start_specific_range_vms(start_number,end_number):
+    _,instances,zones = get_vm_data_csv(VM_DATA_CSV_PATH)
+    [start_command(instance_name,instance_zone) for (instance_name,instance_zone) in zip(instances[start_number-1:end_number],zones[start_number-1:end_number])]
 
 def start_specific_vms(vms_list:list):
     _,instances,zones = get_vm_data_csv(VM_DATA_CSV_PATH)
@@ -80,16 +61,16 @@ def start_specific_vms(vms_list:list):
     [start_command(instance_name,instance_zone) for (instance_name,instance_zone) in chosen_vms_data]
     return chosen_vms_data[0]
 
-def start_specific_range_vms(start_number,end_number):
-    _,instances,zones = get_vm_data_csv(VM_DATA_CSV_PATH)
-    [start_command(instance_name,instance_zone) for (instance_name,instance_zone) in zip(instances[start_number-1:end_number],zones[start_number-1:end_number])]
-
-def start_all_vms(instances,zones):
-    [start_command(instance_name,instance_zone) for (instance_name,instance_zone) in zip(instances,zones)]
-
 def stop_command(instance_name_str,zone_str):
     command = f'gcloud compute instances stop --zone {zone_str}  {instance_name_str}&'
     os.system(command)
+
+def stop_all_vms(instances,zones):
+    [stop_command(instance_name,instance_zone) for (instance_name,instance_zone) in zip(instances,zones)]
+
+def stop_specific_range_vms(start_number,end_number:list):
+    _,instances,zones = get_vm_data_csv(VM_DATA_CSV_PATH)
+    [stop_command(instance_name,instance_zone) for (instance_name,instance_zone) in zip(instances[start_number-1:end_number],zones[start_number-1:end_number])]
 
 def stop_specific_vms(vms_list:list):
     _,instances,zones = get_vm_data_csv(VM_DATA_CSV_PATH)
@@ -99,19 +80,28 @@ def stop_specific_vms(vms_list:list):
     [stop_command(instance_name,instance_zone) for (instance_name,instance_zone) in chosen_vms_data]
     return chosen_vms_data
 
-def stop_specific_range_vms(start_number,end_number:list):
-    _,instances,zones = get_vm_data_csv(VM_DATA_CSV_PATH)
-    [stop_command(instance_name,instance_zone) for (instance_name,instance_zone) in zip(instances[start_number-1:end_number],zones[start_number-1:end_number])]
-
-def stop_all_vms(instances,zones):
-    [stop_command(instance_name,instance_zone) for (instance_name,instance_zone) in zip(instances,zones)]
-
 def ssh_login_and_run_commands_script(df,vm_number,command = SCRIPT_NAME_ON_ALL_VM_BASE_PATH_RUN_COMMAND, project_name_str=PROJECT_NAME):
+
+    # SCRIPT_NAME_ON_ALL_VM_BASE_PATH_RUN_COMMAND = "./pull_docker.sh"
+
     instance_name, zone = get_vm_attributes_from_vm_number(df,vm_number)
     if command is not None : 
         return os.system (f'gcloud beta compute ssh --zone {zone} {instance_name}  --project {project_name_str} --command={command}')
     else : 
         return os.system (f'gcloud beta compute ssh --zone {zone} {instance_name}  --project {project_name_str}')
+
+def ssh_to_machine_via_number(vm_number:int): 
+    return os.system(SSH_COMMAND_BY_VM_NUMBER_DICT[f'{vm_number}'])
+
+def start_and_connect_to_vm(vm_number:int):
+    start_specific_vms([f'{vm_number}'])
+    ssh_to_machine_via_number(vm_number)
+
+
+def get_vm_attributes_from_df_query(df):
+    instance_names,zone_names = df['instance'].to_list(),df['zone'].to_list()
+    vm_data_dict = dict(zip(instance_names, zone_names))
+    return vm_data_dict
 
 def get_vm_attributes_from_vm_number(df, vm_number : int): 
     instance_name = f'instance-' + str(vm_number).zfill(2)
@@ -119,13 +109,23 @@ def get_vm_attributes_from_vm_number(df, vm_number : int):
     zone = df.iloc[instance_idx]['zone'].item()
     return instance_name,zone
 
-def get_all_vm_attributes_from_df_query(df):
-    instance_names,zone_names = df['instance'].to_list(),df['zone'].to_list()
-    vm_data_dict = dict(zip(instance_names, zone_names))
-    return vm_data_dict
+def send_script_to_vm_by_vm_number(df,vm_number,src_path = None , dest_path = None): 
+    #deprecated for now
+    instance_name , zone = get_vm_attributes_from_vm_number(df, vm_number)
+    if src_path == None : src_path = './pull_docker.sh'
+    if dest_path == None : dest_path = f'{instance_name}:~'
+    return os.system(f'gcloud compute scp --project {PROJECT_NAME} --zone {zone} {src_path} {dest_path}')
+
+def send_script_to_all_vms_by_vm_numbers(df):
+    #deprecated for now
+    #currently serial and not parallel
+    df['vm_number'] = df['instance'].apply(lambda x : str(int(x.split(sep='-')[-1].strip())))
+    vm_numbers = df['vm_number'].to_list()
+    [send_script_to_vm_by_vm_number(df,vm_number) for vm_number in vm_numbers]
+
 
 def send_script_to_vm(df,vm_name,src_path = None , dest_path = None):
-    vm_data_dict = get_all_vm_attributes_from_df_query(df)
+    vm_data_dict = get_vm_attributes_from_df_query(df)
     vm_zone = vm_data_dict[f'{vm_name}']
     if src_path == None : src_path = './pull_docker.sh'
     if dest_path == None : dest_path = f'{vm_name}:~'
@@ -162,7 +162,7 @@ def generate_bash_scripts_with_commands(commands):
 def main(): 
 
     df,instances,zones = get_vm_data_csv(VM_DATA_CSV_PATH)
-    vm_data_dict = get_all_vm_attributes_from_df_query(df)
+    vm_data_dict = get_vm_attributes_from_df_query(df)
     
     commands = read_commands_from_txt(COMMANDS_TXT_FILE)
     generate_bash_scripts_with_commands(commands)
